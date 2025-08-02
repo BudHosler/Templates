90bda9f663f922797927f8a4e74d381156d3379b
Gene Hosler <bhosler89@gmail.com>
2025-08-01 21:39:04 -0400

    Created function, missing explicit int type

--- Templates.cpp
+++ Templates.cpp
@@ -5,6 +5,22 @@
 #include <iostream>
 using namespace std;
 
+template <typename T>
+
+T half(T);
+
 int main() {
-	 
+	double a = 7.0;
+	float b = 5.0f;
+	int c = 3;
+
+	cout << half(a) << endl;
+	cout << half(b) << endl;
+	cout << half(c) << endl;
+}
+
+template<typename T>
+T half(T input)
+{
+	return input / 2;
 }
